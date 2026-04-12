c6e9a61823f2905a970870dbba66e174dee2b775
Jamal Sanei <Jamal.sanei@gmail.com>
2026-04-12 18:24:46 +0400

    feat(): list user, add, search

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 const DATA = [
   { id: 1, name: "j" },
@@ -9,11 +9,15 @@ const DATA = [
 const App = () => {
   const [users, setUsers] = useState(DATA);
   const [inputVal, setInputVal] = useState("");
+  const [inputSearch, setInputSearch] = useState("");
+  const [canAdd, setCanAdd] = useState(false);
+  const inputRef = useRef(null);
   const ENTER_KEY_CODE = 13;
 
   const addItem = () => {
     setUsers([...users, { id: users.length + 1, name: inputVal }]);
     setInputVal("");
+    inputRef.current.focus();
   };
 
   const changeInputValue = ({ target }) => setInputVal(target.value);
@@ -23,22 +27,49 @@ const App = () => {
   };
 
   return (
-    <div>
+    <div className=" p-4 rounded-lg mt-10 ml-10 shadow-lg">
       <div>
         <input
+          className="border rounded p-2 mr-2"
           type="text"
-          value={inputVal}
-          onChange={changeInputValue}
-          onKeyUp={handleKeyUp}
+          placeholder="type to searching..."
+          value={inputSearch}
+          onChange={({ target }) => setInputSearch(target.value)}
         />
-        <button onClick={addItem} disabled={!inputVal}>
-          Add
+        <button
+          className="
+          bg-slate-500 rounded border p-2
+          border-blue-500  text-white"
+          onClick={() => setCanAdd(!canAdd)}
+        >
+          +Add Item
         </button>
       </div>
+      {canAdd ? (
+        <div className="bg-slate-100 mb-4 flex p-4 justify-between">
+          <input
+            type="text"
+            value={inputVal}
+            onChange={changeInputValue}
+            onKeyUp={handleKeyUp}
+            ref={inputRef}
+            className="border rounded mr-2 px-2"
+          />
+          <button
+            className="bg-white p-2 rounded cursor-pointer hover:bg-slate-200"
+            onClick={addItem}
+            disabled={!inputVal}
+          >
+            Add
+          </button>
+        </div>
+      ) : null}
       <ul>
-        {users.map(({ id, name }) => (
-          <li key={id}>{name}</li>
-        ))}
+        {users
+          .filter(({ name }) => name.includes(inputSearch))
+          .map(({ id, name }) => (
+            <li key={id}>{name}</li>
+          ))}
       </ul>
     </div>
   );
